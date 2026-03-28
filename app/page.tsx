db79337be8bec5470f5ebd88c310d32300e273e3
Moritz Davinghausen <moritz.davinghausen@uzh.ch>
2026-03-28 09:52:11 +0100

    Remove unused imports in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
-"use client"; // For components that need React hooks and browser APIs, SSR (server side rendering) has to be disabled. Read more here: https://nextjs.org/docs/pages/building-your-application/rendering/server-side-rendering
+"use client";
 import { useRouter } from "next/navigation";
-import Image from "next/image";
-import { Button } from "antd";
-import { BookOutlined, CodeOutlined, GlobalOutlined } from "@ant-design/icons";
-import styles from "@/styles/page.module.css";
 import { GraduationCap, Presentation } from "lucide-react";
 
 
